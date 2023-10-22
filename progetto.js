f0ac3f7bd66656124f6cc3fd2fedda0491c9d4b6
Angelo Antona <angelo.99antona@gmail.com>
2023-10-22 22:45:59 +0200

    MappingKeys

--- progetto.js
+++ progetto.js
@@ -150,38 +150,59 @@ let KickPad = document.getElementById('pad1');
 KickPad.addEventListener('mousedown', () => {
     playKick();
     document.getElementById("display").innerHTML = document.getElementById("display").innerHTML + '<br>' + 'Kick';
-    setTimeout(()=>{document.getElementById("display").innerHTML = defaultString;},500);
+    setTimeout(() => { document.getElementById("display").innerHTML = defaultString; }, 500);
 })
 
 let snarePad = document.getElementById('pad2');
 snarePad.addEventListener('mousedown', () => {
     playSnare();
     document.getElementById("display").innerHTML = document.getElementById("display").innerHTML + '<br>' + 'Snare';
-    setTimeout(()=>{document.getElementById("display").innerHTML = defaultString;},500);
+    setTimeout(() => { document.getElementById("display").innerHTML = defaultString; }, 500);
 })
 
 let closedHitPad = document.getElementById('pad3');
 closedHitPad.addEventListener('mousedown', () => {
     playClosedHiHat();
     document.getElementById("display").innerHTML = document.getElementById("display").innerHTML + '<br>' + 'Closed Hit Hat';
-    setTimeout(()=>{document.getElementById("display").innerHTML = defaultStri1ng;},500);
+    setTimeout(() => { document.getElementById("display").innerHTML = defaultStri1ng; }, 500);
 })
 
 let crashPad = document.getElementById('pad4');
 crashPad.addEventListener('mousedown', () => {
     playCrashCymbal();
     document.getElementById("display").innerHTML = document.getElementById("display").innerHTML + '<br>' + 'Crash Cymbal';
-    setTimeout(()=>{document.getElementById("display").innerHTML = defaultString;},500);
+    setTimeout(() => { document.getElementById("display").innerHTML = defaultString; }, 500);
 })
 
 
-document.addEventListener('keydown',(e)=>{
-    if(e.key=='1') playKick();
-    else if(e.key=='2') playSnare();
-    else if(e.key=='3') playClosedHiHat();
-    else if(e.key=='4') playCrashCymbal();
+// Define a key-to-note mapping object
+const keyToNote = {
+    'z': 1, 's': 2, 'x': 3, 'd': 4, 'c': 5, 'v': 6, 'g': 7, 'b': 8, 'h': 9, 'n': 10, 'j': 11, 'm': 12, ',':13
+};
+
+function getPressedNote(key) {
+    return keyToNote[key] || 0; // Return 0 if the key is not in the mapping
+}
+
+document.addEventListener('keydown', (e) => {
+    if (e.key == '1') playKick();
+    else if (e.key == '2') playSnare();
+    else if (e.key == '3') playClosedHiHat();
+    else if (e.key == '4') playCrashCymbal();
+    else {
+        note = getPressedNote(e.key);
+        if (note&&!pressedKeys[note]) pressedKeys[note] = playNote(note);
+    }
 })
 
+
+document.addEventListener('keyup', (e) => {
+    note = getPressedNote(e.key);
+    if (note) {
+        stopNote(pressedKeys[note]);
+        delete pressedKeys[note];
+    }
+})
 
 
 
@@ -191,11 +212,11 @@ document.addEventListener('keydown',(e)=>{
 const c = new AudioContext();
 const compressor = c.createDynamicsCompressor();
 let attackNote = 0.06;
-let releaseNote = 0.36;
+let releaseNote = 0.10;
 let gainVal = 0;
 
 //analyser_node:
-const analyser= c.createAnalyser();
+const analyser = c.createAnalyser();
 // Set up the AnalyserNode properties
 analyser.fftSize = 256;
 const bufferLength = analyser.frequencyBinCount;
@@ -212,76 +233,78 @@ function getAmplitude() { // Function to get the current amplitude
 // Connect the custom analyser node to the audio output
 compressor.connect(analyser);
 analyser.connect(c.destination);
+let octave=1/2;
 
 
 // PLAYFUNCTIONS -----------------------------------------------------------------------------------------------------------------------------------------------------
 
 function playNote(note) {
     var o = c.createOscillator();
-    o.type = "triangle";
+    o.type = "sawtooth";
     var g = c.createGain();
 
-    o.frequency.value = 261.63 * Math.pow(2, note / 12);
+    o.frequency.value = 261.63*octave * Math.pow(2, note / 12);
     o.connect(g);
     g.connect(compressor);
 
     g.gain.setValueAtTime(0, c.currentTime);
-    g.gain.linearRampToValueAtTime(gainVal, c.currentTime + attackNote);
+    g.gain.linearRampToValueAtTime(gainVal * 0.7, c.currentTime + attackNote);
 
     o.start();
+
     return { o, g };
 }
 
 function stopNote(note) {
-    note.g.gain.linearRampToValueAtTime(0, c.currentTime + releaseNote); // Start the release
-    note.g.gain.linearRampToValueAtTime(0, c.currentTime + releaseNote + 0.01); // Ensure the gain is zero after the release
+    const releaseTime = c.currentTime + releaseNote;
+    const fadeOutDuration = 0.01; // Adjust this duration as needed
+
+    note.g.gain.cancelScheduledValues(c.currentTime); // Cancel any previous automation
+    note.g.gain.setValueAtTime(note.g.gain.value, releaseTime); // Set the current value as the starting point
+    note.g.gain.linearRampToValueAtTime(0, releaseTime + fadeOutDuration);
+
     setTimeout(() => {
         note.o.stop();
         note.o.disconnect();
-    }, c.currentTime + releaseNote + 8000); // 2000ms is a buffer to ensure the sound is stopped
+    }, (releaseNote + fadeOutDuration) * 1000); // Convert seconds to milliseconds
 }
+
 
 function playKick() {
     const duration = 2; // Duration of the kick sound in seconds
-    const attackDuration = 0.02; // Attack duration in seconds
-    const releaseDuration = 0.7; // Release duration in seconds
+    const attackDuration = 0.01; // Attack duration in seconds
+    const releaseDuration = 4; // Release duration in seconds
 
     let buffer = c.createBuffer(1, c.sampleRate * duration, c.sampleRate);
     let bufferData = buffer.getChannelData(0);
-    let initialFrequency = 70;
-    let initialAmplitude = 1;
+    let initialFrequency = 30;
+    let initialAmplitude = 10;
 
     for (let i = 0; i < bufferData.length; i++) {
-        let amplitude = initialAmplitude * Math.exp(-i * (1 / c.sampleRate));
-
-        if (i < c.sampleRate * attackDuration) {
-            // Apply an attack envelope (ramp up) during the attack phase
-            amplitude *= i / (c.sampleRate * attackDuration);
-        } else if (i > c.sampleRate * (duration - releaseDuration)) {
-            // Apply a release envelope (ramp down) during the release phase
-            amplitude *= Math.exp(-(i - c.sampleRate * (duration - releaseDuration)) / (c.sampleRate * releaseDuration));
-        }
-
-        let frequency = initialFrequency * Math.exp(-i * (1 / c.sampleRate));
-        bufferData[i] = (amplitude * Math.cos(2 * Math.PI * frequency * i / c.sampleRate)) * 2 - 1;
+        let amplitude = Math.exp(-i * (initialAmplitude / c.sampleRate));
+        let frequency = 100 * Math.exp(-i * (initialFrequency / c.sampleRate));
+        bufferData[i] = (amplitude * Math.cos(2 * Math.PI * frequency * i / c.sampleRate));
     }
 
     const source = c.createBufferSource();
     source.buffer = buffer;
-    
+
     g = c.createGain();
     g.gain.value = gainVal;
-    
+
     source.connect(g);
     g.connect(compressor);
     source.start(0);
-    
+
     // Schedule the stop of the source node after the duration
     source.stop(c.currentTime + duration);
 }
 
+
+
+
 function playSnare() {
-    const bufferSize = c.sampleRate * 0.1; // Adjust this value for the desired duration (shorter duration for a snappier sound)
+    const bufferSize = c.sampleRate * 0.1;
     const buffer = c.createBuffer(1, bufferSize, c.sampleRate);
     const bufferData = buffer.getChannelData(0);
 
@@ -289,25 +312,28 @@ function playSnare() {
     noiseGain.gain.setValueAtTime(gainVal, c.currentTime);
     noiseGain.gain.linearRampToValueAtTime(0, c.currentTime + bufferSize / c.sampleRate);
 
-    const toneOscillator = c.createOscillator();
-    toneOscillator.type = 'sine'; // You can try different oscillator types (sine, triangle, etc.)
-    toneOscillator.frequency.value = 170; // Adjust this value for the desired pitch
-
-    const toneGain = c.createGain();
-    toneGain.gain.setValueAtTime(gainVal/5, c.currentTime); // Adjust the gain for the tone
-
     // Create white noise for the snare
     for (let i = 0; i < bufferSize; i++) {
         bufferData[i] = (Math.random() * 2 - 1); // Generate random noise
     }
 
-    // Connect and schedule the audio components
-    noiseGain.connect(compressor);
+    const toneOscillator = c.createOscillator();
+    toneOscillator.type = 'sine';
+    toneOscillator.frequency.value = 40; // Adjust this value for the desired pitch
+
+    const toneGain = c.createGain();
+    toneGain.gain.setValueAtTime(gainVal / 5, c.currentTime);
+    toneGain.gain.linearRampToValueAtTime(0, c.currentTime + 0.02); // Adjust the duration of the tone burst
+
+    // Connect the components
+    noiseGain.connect(toneGain.gain);
     toneOscillator.connect(toneGain);
     toneGain.connect(compressor);
 
+    // Start the noise and the tone simultaneously
+    noiseGain.connect(compressor);
     toneOscillator.start();
-    toneOscillator.stop(c.currentTime + bufferSize / c.sampleRate);
+    toneOscillator.stop(c.currentTime + 0.02); // Adjust the duration of the tone burst
 
     const source = c.createBufferSource();
     source.buffer = buffer;
@@ -315,8 +341,9 @@ function playSnare() {
     source.start(0);
 }
 
+
 function playClosedHiHat() {
-    const bufferSize = c.sampleRate * 0.03; // Adjust this value for the desired duration (shorter duration for a crisp sound)
+    const bufferSize = c.sampleRate * 0.03;
     const buffer = c.createBuffer(1, bufferSize, c.sampleRate);
     const bufferData = buffer.getChannelData(0);
 
@@ -329,8 +356,15 @@ function playClosedHiHat() {
         bufferData[i] = (Math.random() * 2 - 1); // Generate random noise
     }
 
-    // Connect and schedule the audio components
-    noiseGain.connect(compressor);
+    // Create a band-pass filter
+    const filter = c.createBiquadFilter();
+    filter.type = "bandpass";
+    filter.frequency.value = 3000; // Adjust this value for the desired pitch
+    filter.Q.value = 1; // Adjust this value for the filter resonance
+
+    // Connect the components
+    noiseGain.connect(filter);
+    filter.connect(compressor);
 
     const source = c.createBufferSource();
     source.buffer = buffer;
@@ -341,13 +375,14 @@ function playClosedHiHat() {
 
 
 
+
 function playCrashCymbal() {
-    const bufferSize = c.sampleRate * 1.5; // Adjust this value for the desired duration
+    const bufferSize = c.sampleRate * 1.; // Adjust this value for the desired duration
     const buffer = c.createBuffer(1, bufferSize, c.sampleRate);
     const bufferData = buffer.getChannelData(0);
 
     const noiseGain = c.createGain();
-    noiseGain.gain.setValueAtTime(gainVal*0.7, c.currentTime);
+    noiseGain.gain.setValueAtTime(gainVal * 0.5, c.currentTime);
     noiseGain.gain.linearRampToValueAtTime(0, c.currentTime + bufferSize / c.sampleRate);
 
     // Create white noise for the crash cymbal
@@ -355,8 +390,15 @@ function playCrashCymbal() {
         bufferData[i] = (Math.random() * 2 - 1); // Generate random noise
     }
 
-    // Connect and schedule the audio components
-    noiseGain.connect(compressor);
+    // Create a low-pass filter to shape the cymbal sound
+    const filter = c.createBiquadFilter();
+    filter.type = "lowpass";
+    filter.frequency.setValueAtTime(9900, c.currentTime); // Adjust this value for the desired tone
+    filter.Q.setValueAtTime(1, c.currentTime);
+
+    // Connect the components
+    noiseGain.connect(filter);
+    filter.connect(compressor);
 
     const source = c.createBufferSource();
     source.buffer = buffer;
@@ -400,34 +442,34 @@ knobs.forEach((knob) => {
 });
 // SINUSOID ------------------------------------------------------------------------------------------------------------------------------------------------
 const canvas = document.getElementById('signalCanvas');
-        const ctx = canvas.getContext('2d');
-        const frequency = 0.02;
-        let phase = 0;
-        const centerX = canvas.width / 2;
-        
-        function drawSinusoidalSignal() {
-            const width = canvas.width;
-            const height = canvas.height;
-            
-            ctx.clearRect(0, 0, width, height);
-            
-            ctx.beginPath();
-            ctx.strokeStyle = 'k';
-            ctx.lineWidth = 10;
+const ctx = canvas.getContext('2d');
+const frequency = 0.02;
+let phase = 0;
+const centerX = canvas.width / 2;
 
-            for (let x = 0; x < width; x++) {
-                const currentAmplitude = getAmplitude();
-                const y = height / 2 + Math.sin(frequency * x + phase) * 200*currentAmplitude;
-                ctx.lineTo(x, y);
-            }
+function drawSinusoidalSignal() {
+    const width = canvas.width;
+    const height = canvas.height;
 
-            ctx.stroke();
-        }
+    ctx.clearRect(0, 0, width, height);
 
-        function animate() {
-            phase += 0.1; // Adjust the animation speed
-            drawSinusoidalSignal();
-            requestAnimationFrame(animate);
-        }
+    ctx.beginPath();
+    ctx.strokeStyle = 'k';
+    ctx.lineWidth = 10;
 
-        animate();
+    for (let x = 0; x < width; x++) {
+        const currentAmplitude = getAmplitude();
+        const y = height / 2 + Math.sin(frequency * x + phase) * 200 * currentAmplitude;
+        ctx.lineTo(x, y);
+    }
+
+    ctx.stroke();
+}
+
+function animate() {
+    phase += 0.1; // Adjust the animation speed
+    drawSinusoidalSignal();
+    requestAnimationFrame(animate);
+}
+
+animate();
